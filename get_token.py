ddfed3cb0d35e1ac62d8d5e3f1799f61b214d7a2
Thungghuan <1092887426@qq.com>
2022-09-18 17:25:22 +0800

    feat: generate token file

---
+++ get_token.py
@@ -0,0 +1,34 @@
+import re
+import requests
+from des import str_enc
+
+
+def get_token(username: str, password: str):
+
+    # A fake user agent
+    user_agent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2"
+
+    session = requests.session()
+    session.trust_env = False
+    session.headers["User-Agent"] = user_agent
+
+    # This will be rediect to the login page
+    redirect_login_page = session.get("https://life.gzic.scut.edu.cn/")
+
+    lt_pattern = re.compile(r'<input.+name="lt"\s+value="([^"]+)"')
+    lt = re.search(lt_pattern, redirect_login_page.text).group(1)
+
+    login_post_param = {
+        "ul": len(username),
+        "pl": len(password),
+        "lt": lt,
+        "rsa": str_enc(username + password + lt, "1", "2", "3"),
+        "execution": "e1s1",
+        "_eventId": "submit",
+    }
+
+    session.post(url="https://sso.scut.edu.cn/cas/login", data=login_post_param)
+
+    token = session.get("https://life.gzic.scut.edu.cn/auth/login/cas/token").json()
+
+    return token["data"]
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('Hello, GZIC')
+from get_token import get_token
+import os.path as path
+
+print("gzic_bus / gzic校巴预约")
+
+token = ""
+print("读取token文件中...")
+
+if path.exists("token"):
+    with open("token") as f:
+        token = f.read().strip()
+        print("token读取成功")
+else:
+    print("请先使用统一认证账号登陆获取token")
+    username = input("学号：")
+    password = input("密码：")
+
+    token = get_token(username, password)
+    print("登陆成功，写入token文件")
+
+    with open("token", "w+") as f:
+        f.write(token)
