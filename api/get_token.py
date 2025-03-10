cd1b18c576e05ef5337177804345d9d9ab2dc795
Thungghuan <1092887426@qq.com>
2025-03-10 13:58:40 +0800

    feat: add qrcode login

--- api/get_token.py
+++ api/get_token.py
@@ -1,14 +1,10 @@
+import os
 import re
 import requests
+from urllib.parse import quote
 from api.des import str_enc
 
 
-# TODO: QR_code
-# 1. generate qrcode: qrcode.makeCode("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx39f121ed798af736&redirect_uri=https%3A%2F%2Fsso.scut.edu.cn%2Fcas%2Fscutwxsso&response_type=code&scope=snsapi_base&state="+uuid+"#wechat_redirect");
-# 2. check scan "https://sso.scut.edu.cn/cas/scutqqcheck?uuid=" + uuid, get token
-# 3. window.location.href="qRCode?token="+token+"&"+queryString; redirect with token, get tickie from headers.Location
-# 4. now can get token of gzic_bus
-# 5. qrcode expired in 2min
 class LoginSession:
     user_agent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2"
 
@@ -45,10 +41,12 @@ class LoginSession:
         else:
             return False, None
 
-    # TODO: CAPTCHA
-    # 1. wait for input captcha manually
-    # 2. login again and get token
     def relogin(self, username: str, password: str, captcha: str, lt: str):
+        """#CAPTCHA
+        1. wait for input captcha manually
+        2. login again and get token
+        """
+
         login_post_param = {
             "PM1": captcha,
             "ul": len(username),
@@ -64,6 +62,74 @@ class LoginSession:
         )
 
         return "PM1" not in login_page
+
+    def gen_uuid(self):
+        import time
+        import random
+
+        ts = round(time.time() * 1000)
+        uuid = ""
+        for ch in "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx":
+            r = int((ts + random.random() * 16) % 16)
+            if ch == "x":
+                uuid += hex(r)[2:]
+                ts //= 16
+            elif ch == "y":
+                uuid += hex(r & 0x3 | 0x8)[2:]
+                ts //= 16
+            else:
+                uuid += ch
+
+        return uuid
+
+    def gen_qrcode(self):
+        """# QR_code
+        1. generate qrcode: qrcode.makeCode("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx39f121ed798af736&redirect_uri=https%3A%2F%2Fsso.scut.edu.cn%2Fcas%2Fscutwxsso&response_type=code&scope=snsapi_base&state="+uuid+"#wechat_redirect");
+        2. check scan "https://sso.scut.edu.cn/cas/scutqqcheck?uuid=" + uuid, get token
+        3. window.location.href="qRCode?token="+token+"&"+queryString; redirect with token, get tickie from headers.Location
+        4. now can get token of gzic_bus
+        5. qrcode expired in 2min
+        """
+
+        import qrcode
+
+        uuid = self.gen_uuid()
+        url = (
+            "https://open.weixin.qq.com/connect/oauth2/authorize"
+            + "?appid=wx39f121ed798af736"
+            + f"&redirect_uri={quote('https://sso.scut.edu.cn/cas/scutwxsso', safe='')}"
+            + "&response_type=code"
+            + "&scope=snsapi_base"
+            + f"&state={uuid}"
+            + "#wechat_redirect"
+        )
+
+        login_qrcode = qrcode.QRCode(box_size=2)
+        login_qrcode.add_data(url)
+        qrcode_img = login_qrcode.make_image()
+
+        save_path = os.path.join(os.getcwd(), "qrcode.jpg")
+        qrcode_img.save(save_path)
+
+        return save_path, uuid
+
+    def check_qrcode_scan(self, uuid):
+        url = "https://sso.scut.edu.cn/cas/scutqqcheck?uuid=" + uuid
+        result = self.session.get(url)
+
+        resp_pattern = re.compile(r'null\("(..+)"\)')
+        token_match = re.search(resp_pattern, result.text)
+        if token_match:
+            token = token_match[1]
+            self.session.get(
+                "https://sso.scut.edu.cn/cas/qRCode"
+                + f"?token={token}"
+                + f"&service={quote('https://life.gzic.scut.edu.cn/login/cas/', safe='')}"
+            )
+
+            return True
+        else:
+            return False
 
     def get_token(self):
         # Post first, then can get the token
--- cli/token.py
+++ cli/token.py
@@ -1,11 +1,12 @@
 import questionary
 import os.path as path
+import time
 from api.get_token import check_token_expired, LoginSession
 
 TOKEN_PATH = ".token"
 
 
-def load_token(username = None, password = None):
+def load_token(username=None, password=None):
     token = ""
     print("读取token文件中...")
 
@@ -24,23 +25,52 @@ def load_token(username = None, password = None):
     return token
 
 
-def login(username = None, password = None):
-    if not username or not password:
-        print("请先使用统一认证账号登陆获取token")
-        username = questionary.text("学号：").ask()
-        password = questionary.password("密码：").ask()
+def login(username=None, password=None):
+    print("请先使用统一认证账号登陆获取token")
+    useQRCode = questionary.select(
+        "请选择登陆方式",
+        choices=[
+            {"name": "(1) 账号密码登录", "value": False},
+            {"name": "(2) 二维码登录", "value": True},
+        ],
+    ).ask()
 
-    if not username or not password:
-        print("请输入用户名和密码")
-        exit()
-
-    # token = get_token(username, password)
     login_sess = LoginSession()
-    need_captcha, lt = login_sess.login(username, password)
+    if useQRCode:
+        qrcode_path, uuid = login_sess.gen_qrcode()
+        print(f"请扫描二维码登录： {qrcode_path} (有效期2分钟)")
 
-    if need_captcha:
-        captcha = questionary.text("验证码：").ask()
-        login_sess.relogin(username, password, captcha, lt)
+        start_time = round(time.time())
+        while True:
+            print(".", end="", flush=True)
+            scan_status = login_sess.check_qrcode_scan(uuid)
+
+            if scan_status:
+                break
+            else:
+                curr_time = round(time.time())
+                if curr_time - start_time > 120:
+                    start_time = round(time.time())
+                    qrcode_path, uuid = login_sess.gen_qrcode()
+                    print("")
+                    print("二维码过期，刷新二维码")
+                else:
+                    time.sleep(2)
+
+    else:
+        if not username or not password:
+            username = questionary.text("学号：").ask()
+            password = questionary.password("密码：").ask()
+
+        if not username or not password:
+            print("请输入用户名和密码")
+            exit()
+
+        need_captcha, lt = login_sess.login(username, password)
+
+        if need_captcha:
+            captcha = questionary.text("验证码：").ask()
+            login_sess.relogin(username, password, captcha, lt)
 
     token = login_sess.get_token()
     print("登陆成功，写入token文件")
